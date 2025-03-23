fd38960dd23d9253d8b9bebfa12aec8c56ed0570
Evgen <polivanov004@gmail.com>
2025-03-23 13:11:20 +0300

    Добавьте файлы проекта.

---
+++ MathOperations.cs
@@ -0,0 +1,33 @@
+﻿namespace ClassLibrary1
+{
+    public class MathOperations
+    {
+        // Addition
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Subtraction
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        // Multiplication
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        // Division
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return (double)a / b;
+        }
+    }
+}
